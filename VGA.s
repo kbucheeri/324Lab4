c6ac7bb40f592be6c974b4acf4f639d49bed799a
kbucheeri <khaled.bucheeri@mail.mcgill.ca>
2020-04-04 17:37:43 -0400

    write char subroutine works

--- VGA.s
+++ VGA.s
@@ -2,12 +2,21 @@
 .global _start
 .equ CHAR_BUFF_BASE, 0xC9000000
 .equ PIXEL_BUFF_BASE, 0XC8000000
+CHAR_TABLE:
+.space 
 .global VGA_clear_char_buff_ASM
 .global VGA_clear_pixel_buff_ASM
 .global VGA_write_char_ASM
 .global VGA_write_byte_ASM
 .global VGA_draw_point_ASM
 _start:
+; MOV R0, #65
+; MOV R1, #40
+; MOV R2, #65
+; BL VGA_write_char_ASM
+
+
+
 VGA_clear_char_buff_ASM:		//TODO: add callee save convention.
 	
 	MOV R0, #60				//OUTER LOOP COUNTER. I'll left shift, then add. since there are 240 y pixels. 
@@ -29,7 +38,7 @@ INNER_LOOP_C:
 INNER_LOOP_DONE_C:
 	CMP R0, #0
 	BGT OUTER_LOOP_C
-	B INNER_LOOP_DONE_C
+	BX LR
 
 
 VGA_clear_pixel_buff_ASM:
@@ -52,7 +61,37 @@ INNER_LOOP_P:
 INNER_LOOP_DONE_P:
 	CMP R0, #0
 	BGT OUTER_LOOP_P
-	B INNER_LOOP_DONE_P
+	BX LR
+
+
 VGA_write_char_ASM:
+	PUSH {R3}	//will be used to hold the address, do callee save
+	PUSH {R1}	//will be left shifted, so apply callee save
+	//LSL R0, #1	//SHIFT THE X positions
+	LSL R1, #7	//shift the y positions
+	LDR R3, =CHAR_BUFF_BASE
+	ADD R3, R3, R1
+	ADD R3, R3, R0 //add the x and y offsets the buffer base
+	STRB R2, [R3]
+	POP {R1}
+	POP {R3}
+	BX LR
+
+
+
+
 VGA_write_byte_ASM:
+	PUSH {R3}	//will be used to hold the address, do callee save
+	PUSH {R1}	//will be left shifted, so apply callee save
+	//LSL R0, #1	//SHIFT THE X positions
+	LSL R1, #7	//shift the y positions
+	LDR R3, =CHAR_BUFF_BASE
+	ADD R3, R3, R1
+	ADD R3, R3, R0 //add the x and y offsets the buffer base
+	STRB R2, [R3]
+	POP {R1}
+	POP {R3}
+	BX LR
+
+	
 VGA_draw_point_ASM:
